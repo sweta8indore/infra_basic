8bf4fa8c48cbc0426040646a29b1897406b11b8a
Sweta Mishra <44045993+sweta8indore@users.noreply.github.com>
2026-04-13 12:18:27 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "4.62.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "rg-state"
+    storage_account_name = "terraformstatefile55"
+    container_name = "tfstate"
+    key = "dev.terraform.state"
+  }
 }
 provider "azurerm" {
   features {}
