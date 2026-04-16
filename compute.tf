f4d3155ecc74eeab1727aa301928dfbabeb42fa4
Sweta Mishra <44045993+sweta8indore@users.noreply.github.com>
2026-04-16 12:32:25 +0530

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -1,36 +1,36 @@
 resource "azurerm_network_interface" "nic" {
-  name                = "test-nic"
-  location            = azurerm_resource_group.test-rg.location
-  resource_group_name = azurerm_resource_group.test-rg.name
+#   name                = "test-nic"
+#   location            = azurerm_resource_group.test-rg.location
+#   resource_group_name = azurerm_resource_group.test-rg.name
 
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.subnet.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+# }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "test-vm"
-  resource_group_name = azurerm_resource_group.test-rg.name
-  location            = azurerm_resource_group.test-rg.location
-  size                = "Standard_F2"
-  disable_password_authentication = false
-  admin_username      = "adminuser"
-  admin_password = "admin*123"
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
+# resource "azurerm_linux_virtual_machine" "vm" {
+#   name                = "test-vm"
+#   resource_group_name = azurerm_resource_group.test-rg.name
+#   location            = azurerm_resource_group.test-rg.location
+#   size                = "Standard_F2"
+#   disable_password_authentication = false
+#   admin_username      = "adminuser"
+#   admin_password = "admin*123"
+#   network_interface_ids = [
+#     azurerm_network_interface.nic.id
+#   ]
 
-    os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#     os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
 
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
+#   source_image_reference {
+#     publisher = "Canonical"
+#     offer     = "0001-com-ubuntu-server-jammy"
+#     sku       = "22_04-lts"
+#     version   = "latest"
+#   }
+# }
